7b68e9ffc19ffa5064f33d824fbe493236b769bf
FaizulAmaly <faizuloke@gmail.com>
2020-01-01 00:20:44 +0700

    add unit test

--- src/api/controllers/repositories/repositories_controller_test.go
+++ src/api/controllers/repositories/repositories_controller_test.go
@@ -1,21 +1,29 @@
 package repositories
 
 import (
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"github.com/ilamazuliaf/golang-microservices/src/api/clients/restclient"
+	"github.com/ilamazuliaf/golang-microservices/src/api/domain/repositories"
 	"github.com/ilamazuliaf/golang-microservices/src/api/utils/errors"
+	"github.com/ilamazuliaf/golang-microservices/src/api/utils/test_utils"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 )
 
-func TestCreateRepoInvalidJasonRequest(t *testing.T)  {
-	response := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(response)
+func TestMain(m *testing.M) {
+	restclient.StartMockups()
+	os.Exit(m.Run())
+}
 
+func TestCreateRepoInvalidJasonRequest(t *testing.T) {
 	request, _ := http.NewRequest(http.MethodPost, "/repositories", strings.NewReader(``))
-	c.Request = request
+	response := httptest.NewRecorder()
+	c := test_utils.GetMockedContext(request, response)
 
 	CreateRepo(c)
 
@@ -28,6 +36,59 @@ func TestCreateRepoInvalidJasonRequest(t *testing.T)  {
 	assert.EqualValues(t, "invalid json body", apiErr.Message())
 }
 
-func TestCreateRepoErrorFromGithub(t *testing.T)  {
-	// TODO: next time
+func TestCreateRepoErrorFromGithub(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body: ioutil.NopCloser(strings.NewReader(`
+				{
+				"message":"Requires authentication",
+				"documentation_url":"https://developer.github.com/docs"
+				}`)),
+		},
+	})
+
+	request, _ := http.NewRequest(http.MethodPost, "/repositories", strings.NewReader(`{"name":"testing"}`))
+	response := httptest.NewRecorder()
+	c := test_utils.GetMockedContext(request, response)
+
+	CreateRepo(c)
+
+	assert.EqualValues(t, http.StatusUnauthorized, response.Code)
+
+	apiErr, err := errors.NewApiErrFromBytes(response.Body.Bytes())
+	assert.Nil(t, err)
+	assert.NotNil(t, apiErr)
+	assert.EqualValues(t, http.StatusUnauthorized, apiErr.Status())
+	assert.EqualValues(t, "Requires authentication", apiErr.Message())
+}
+
+func TestCreateRepoNoError(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body: ioutil.NopCloser(strings.NewReader(`{"id":123}`)),
+		},
+	})
+
+	request, _ := http.NewRequest(http.MethodPost, "/repositories", strings.NewReader(`{"name":"testing"}`))
+	response := httptest.NewRecorder()
+	c := test_utils.GetMockedContext(request, response)
+
+	CreateRepo(c)
+
+	assert.EqualValues(t, http.StatusCreated, response.Code)
+
+	var result repositories.CreateRepoResponse
+	err := json.Unmarshal(response.Body.Bytes(), &result)
+	assert.Nil(t, err)
+	assert.EqualValues(t, 123, result.Id)
+	assert.EqualValues(t, "", result.Name)
+	assert.EqualValues(t, "", result.Owner)
 }
