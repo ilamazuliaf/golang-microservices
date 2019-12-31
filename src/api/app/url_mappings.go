6c7b612e35c0ec4a37ffeaa0903f59e50ccafc1e
FaizulAmaly <faizuloke@gmail.com>
2020-01-01 00:20:26 +0700

    add router luffy

--- src/api/app/url_mappings.go
+++ src/api/app/url_mappings.go
@@ -1,9 +1,11 @@
 package app
 
 import (
+	"github.com/ilamazuliaf/golang-microservices/src/api/controllers/luffy"
 	"github.com/ilamazuliaf/golang-microservices/src/api/controllers/repositories"
 )
 
 func mapUrls() {
+	router.GET("/luffy", luffy.Luffy)
 	router.POST("/repositories", repositories.CreateRepo)
 }
