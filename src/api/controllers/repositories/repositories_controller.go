6583eac9cb6e1feb9fdac7be01155c00c291156f
FaizulAmaly <faizuloke@gmail.com>
2020-01-01 11:06:04 +0700

    add createRepos(Concurrent)

--- src/api/controllers/repositories/repositories_controller.go
+++ src/api/controllers/repositories/repositories_controller.go
@@ -23,3 +23,19 @@ func CreateRepo(c *gin.Context) {
 	}
 	c.JSON(http.StatusCreated, result)
 }
+
+func CreateRepos(c *gin.Context) {
+	var request []repositories.CreateRepoRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		apiErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(apiErr.Status(), apiErr)
+		return
+	}
+
+	result, err := services.RepositoryService.CreateRepos(request)
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(result.StatusCode, result)
+}
