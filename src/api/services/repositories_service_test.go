d67ccfb9032c3d2c9ab174678676e1b201c642d6
FaizulAmaly <faizuloke@gmail.com>
2020-01-01 12:29:03 +0700

    add testing concurrent models

--- src/api/services/repositories_service_test.go
+++ src/api/services/repositories_service_test.go
@@ -3,11 +3,13 @@ package services
 import (
 	"github.com/ilamazuliaf/golang-microservices/src/api/clients/restclient"
 	"github.com/ilamazuliaf/golang-microservices/src/api/domain/repositories"
+	"github.com/ilamazuliaf/golang-microservices/src/api/utils/errors"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -67,4 +69,111 @@ func TestCreateRepoNoError(t *testing.T) {
 	assert.EqualValues(t, 123, result.Id)
 	assert.EqualValues(t, "testing", result.Name)
 	assert.EqualValues(t, "ilamazuliaf", result.Owner)
+}
+
+func TestCreateRepoConcurrentInvalidRequest(t *testing.T) {
+	request := repositories.CreateRepoRequest{}
+
+	output := make(chan repositories.CreateRepositoriesResult)
+
+	service := reposService{}
+	go service.createRepoConcurrent(request, output)
+
+	result := <-output
+	assert.NotNil(t, result)
+	assert.Nil(t, result.Response)
+	assert.NotNil(t, result.Error)
+	assert.EqualValues(t, http.StatusBadRequest, result.Error.Status())
+	assert.EqualValues(t, "invalid repository name", result.Error.Message())
+}
+
+func TestCreateRepoConcurrentErrorFromGithub(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body: ioutil.NopCloser(strings.NewReader(`
+				{
+				"message":"Requires authentication",
+				"documentation_url":"https://developer.github.com/docs"
+				}`)),
+		},
+	})
+
+	request := repositories.CreateRepoRequest{Name: "testing"}
+
+	output := make(chan repositories.CreateRepositoriesResult)
+
+	service := reposService{}
+	go service.createRepoConcurrent(request, output)
+
+	result := <-output
+	assert.NotNil(t, result)
+	assert.Nil(t, result.Response)
+	assert.NotNil(t, result.Error)
+	assert.EqualValues(t, http.StatusUnauthorized, result.Error.Status())
+	assert.EqualValues(t, "Requires authentication", result.Error.Message())
+}
+
+func TestCreateRepoConcurrentNoError(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id":123,"name":"testing","owner":{"login":"ilamazuliaf"}}`)),
+		},
+	})
+
+	request := repositories.CreateRepoRequest{Name: "testing"}
+
+	output := make(chan repositories.CreateRepositoriesResult)
+
+	service := reposService{}
+	go service.createRepoConcurrent(request, output)
+
+	result := <-output
+	assert.NotNil(t, result)
+	assert.Nil(t, result.Error)
+	assert.NotNil(t, result.Response)
+	assert.EqualValues(t, 123, result.Response.Id)
+	assert.EqualValues(t, "testing", result.Response.Name)
+	assert.EqualValues(t, "ilamazuliaf", result.Response.Owner)
+}
+
+func TestHandleRepoResults(t *testing.T) {
+	input := make(chan repositories.CreateRepositoriesResult)
+	output := make(chan repositories.CreateReposResponse)
+
+	var wg sync.WaitGroup
+
+	service := reposService{}
+	go service.handleRepoResults(&wg, input, output)
+
+	wg.Add(1)
+
+	go func() {
+		input <- repositories.CreateRepositoriesResult{
+			Error:    errors.NewBadRequestError("invalid repository name"),
+		}
+	}()
+
+	wg.Wait()
+	close(input)
+
+	result := <- output
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 0, result.StatusCode)
+
+	assert.EqualValues(t, 1, len(result.Results))
+	assert.NotNil(t, result.Results[0].Error)
+	assert.EqualValues(t, http.StatusBadRequest, result.Results[0].Error.Status())
+	assert.EqualValues(t, "invalid repository name", result.Results[0].Error.Message())
+}
+
+func TestCreateReposSingleRequest(t *testing.T){
+	// TODO: create test for create repos single request
 }
